4347434e153c394a2f55e776eb568390c5481bc0
Jay.M.Hu <hm910705@163.com>
2015-06-17 23:29:08 +0800

    Add image and use gdi+

---
+++ RoseLove/FrmMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace RoseLove
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(403, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 361);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1157, 623);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "玫瑰之恋";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FrmMain_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RoseLove/FrmMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoseLove
+{
+    public partial class FrmMain : Form
+    {
+        private string path = Path.Combine(Application.StartupPath, "../../Images/qq.png");
+        public FrmMain()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void FrmMain_Paint(object sender, PaintEventArgs e)
+        {
+           
+            Graphics g = e.Graphics;
+            TextureBrush b2 = new TextureBrush(BuildBitmap(path, 20));
+          //  b2.RotateTransform(20);
+            Rectangle rect = new Rectangle(500, 300, 60, 92);//定义矩形,参数为起点横纵坐标以及其长和宽
+            g.FillRectangle(b2, rect);
+
+        }
+
+        public Image BuildBitmap(string path,float py)
+        {
+            Bitmap bitmap =new Bitmap(100,100);
+
+             var g = Graphics.FromImage(bitmap);
+             g.Clear(Color.White);
+             //设置画板的坐标原点为中点
+             g.TranslateTransform(bitmap.Width/2, bitmap.Height/2);
+             //以指定角度对画板进行旋转
+             g.RotateTransform(py);
+             //把数字画到画板的中点位置
+             g.DrawImage(Image.FromFile(path), new Rectangle(10, 10,90,90));
+             return bitmap;
+         }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = BuildBitmap(path, 30);
+            //new Thread(() =>
+            //{ while (pictureBox1.Top < 800)
+            //    {
+            //        Thread.Sleep(50);
+            //        pictureBox1.Top += 2;
+            //    }
+
+            //}).Start();
+        }
+    }
+}
