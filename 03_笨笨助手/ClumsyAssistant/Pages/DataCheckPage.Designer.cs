237acd22dfe9fbbed670486030ddaae4e25b7ea9
Jay.M.Hu <hm910705@163.com>
2015-07-04 01:02:00 +0800

    完成校对功能。

--- 03_笨笨助手/ClumsyAssistant/Pages/DataCheckPage.Designer.cs
+++ 03_笨笨助手/ClumsyAssistant/Pages/DataCheckPage.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.TbFile = new System.Windows.Forms.TextBox();
-            this.TbFile2 = new System.Windows.Forms.TextBox();
-            this.BtnSelectFile = new System.Windows.Forms.Button();
-            this.BtnSelectFile2 = new System.Windows.Forms.Button();
             this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnSelectFile2 = new System.Windows.Forms.Button();
+            this.BtnSelectFile = new System.Windows.Forms.Button();
+            this.TbFile2 = new System.Windows.Forms.TextBox();
+            this.TbFile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RtbLog = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,6 +52,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.RtbLog);
             this.groupBox1.Controls.Add(this.BtnStart);
             this.groupBox1.Controls.Add(this.BtnSelectFile2);
             this.groupBox1.Controls.Add(this.BtnSelectFile);
@@ -60,52 +62,20 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(17, 17);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(360, 146);
+            this.groupBox1.Size = new System.Drawing.Size(360, 432);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "出/入库数据校对";
             // 
-            // groupBox2
+            // BtnStart
             // 
-            this.groupBox2.Location = new System.Drawing.Point(400, 17);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(360, 146);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox1";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 65);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 12);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "请选择试剂部：";
-            // 
-            // TbFile
-            // 
-            this.TbFile.Location = new System.Drawing.Point(115, 30);
-            this.TbFile.Name = "TbFile";
-            this.TbFile.Size = new System.Drawing.Size(169, 21);
-            this.TbFile.TabIndex = 1;
-            // 
-            // TbFile2
-            // 
-            this.TbFile2.Location = new System.Drawing.Point(115, 61);
-            this.TbFile2.Name = "TbFile2";
-            this.TbFile2.Size = new System.Drawing.Size(169, 21);
-            this.TbFile2.TabIndex = 2;
-            // 
-            // BtnSelectFile
-            // 
-            this.BtnSelectFile.Location = new System.Drawing.Point(285, 29);
-            this.BtnSelectFile.Name = "BtnSelectFile";
-            this.BtnSelectFile.Size = new System.Drawing.Size(32, 23);
-            this.BtnSelectFile.TabIndex = 2;
-            this.BtnSelectFile.Text = "...";
-            this.BtnSelectFile.UseVisualStyleBackColor = true;
-            this.BtnSelectFile.Click += new System.EventHandler(this.BtnSelectFile_Click);
+            this.BtnStart.Location = new System.Drawing.Point(242, 105);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(75, 23);
+            this.BtnStart.TabIndex = 3;
+            this.BtnStart.Text = "开始校对";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
             // 
             // BtnSelectFile2
             // 
@@ -117,15 +87,57 @@
             this.BtnSelectFile2.UseVisualStyleBackColor = true;
             this.BtnSelectFile2.Click += new System.EventHandler(this.BtnSelectFile2_Click);
             // 
-            // BtnStart
+            // BtnSelectFile
             // 
-            this.BtnStart.Location = new System.Drawing.Point(242, 105);
-            this.BtnStart.Name = "BtnStart";
-            this.BtnStart.Size = new System.Drawing.Size(75, 23);
-            this.BtnStart.TabIndex = 3;
-            this.BtnStart.Text = "开始处理";
-            this.BtnStart.UseVisualStyleBackColor = true;
-            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            this.BtnSelectFile.Location = new System.Drawing.Point(285, 29);
+            this.BtnSelectFile.Name = "BtnSelectFile";
+            this.BtnSelectFile.Size = new System.Drawing.Size(32, 23);
+            this.BtnSelectFile.TabIndex = 2;
+            this.BtnSelectFile.Text = "...";
+            this.BtnSelectFile.UseVisualStyleBackColor = true;
+            this.BtnSelectFile.Click += new System.EventHandler(this.BtnSelectFile_Click);
+            // 
+            // TbFile2
+            // 
+            this.TbFile2.Location = new System.Drawing.Point(115, 61);
+            this.TbFile2.Name = "TbFile2";
+            this.TbFile2.Size = new System.Drawing.Size(169, 21);
+            this.TbFile2.TabIndex = 2;
+            // 
+            // TbFile
+            // 
+            this.TbFile.Location = new System.Drawing.Point(115, 30);
+            this.TbFile.Name = "TbFile";
+            this.TbFile.Size = new System.Drawing.Size(169, 21);
+            this.TbFile.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "请选择试剂部：";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(400, 17);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(360, 146);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox1";
+            // 
+            // RtbLog
+            // 
+            this.RtbLog.BackColor = System.Drawing.Color.Black;
+            this.RtbLog.ForeColor = System.Drawing.Color.Red;
+            this.RtbLog.Location = new System.Drawing.Point(9, 134);
+            this.RtbLog.Name = "RtbLog";
+            this.RtbLog.Size = new System.Drawing.Size(345, 292);
+            this.RtbLog.TabIndex = 2;
+            this.RtbLog.Text = "日志信息：";
             // 
             // DataCheckPage
             // 
@@ -154,5 +166,6 @@
         private System.Windows.Forms.Button BtnSelectFile2;
         private System.Windows.Forms.Button BtnSelectFile;
         private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.RichTextBox RtbLog;
     }
 }
--- 03_笨笨助手/ClumsyAssistant/Pages/DataCheckPage.cs
+++ 03_笨笨助手/ClumsyAssistant/Pages/DataCheckPage.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Linq;
 using NPOI.HSSF.UserModel;
+using NPOI.HSSF.Util;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
@@ -15,6 +16,8 @@ namespace ClumsyAssistant.Pages
 {
     public partial class DataCheckPage : UserControl
     {
+        const int START_ROW_INDEX = 1; //为什么不是0？因为首行是列头
+        private readonly StringBuilder logBuilder = new StringBuilder();
         public DataCheckPage()
         {
             InitializeComponent();
@@ -36,62 +39,122 @@ namespace ClumsyAssistant.Pages
             MessageBox.Show(msg);
         }
 
+        private void AddLog(string log)
+        {
+            logBuilder.AppendFormat("{0} {1}{2}", DateTime.Now.ToString("HH:mm:ss"), log, Environment.NewLine);
+            RtbLog.Text = logBuilder.ToString();
+        }
+
+        private int GetDocumentNumberColIndex(ISheet sheet)
+        {
+            var cells = sheet.GetRow(0).Cells;
+            for (int i = 0; i < cells.Count; i++)
+            {
+                if (cells[i].StringCellValue.StartsWith("单据编号"))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private IWorkbook GetWorkbook(string filepath)
+        {
+            var fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            if (filepath.IndexOf(".xlsx", StringComparison.Ordinal) > 0) // 2007版本
+            {
+                return new XSSFWorkbook(fs);
+            }
+            else if (filepath.IndexOf(".xls", StringComparison.Ordinal) > 0) // 2003版本
+            {
+                return new HSSFWorkbook(fs);
+            }
+            return null;
+        }
+
         /// <summary>
         /// 根据Excel文件路径，获取单据编号
         /// </summary>
         /// <param name="filepath"></param>
+        /// <param name="isSjbWorkbook"></param>
         /// <returns></returns>
         private List<string> GetDocumentNumber(string filepath)
         {
             var results = new List<string>();
             IWorkbook workbook = null;
+            FileStream fs = null;
             try
             {
-                var fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
-                if (filepath.IndexOf(".xlsx", StringComparison.Ordinal) > 0) // 2007版本
+                workbook = this.GetWorkbook(filepath);
+                var sheet = workbook.GetSheetAt(0);
+                int colIndex = this.GetDocumentNumberColIndex(sheet);
+                if (colIndex < 0)
                 {
-                    workbook = new XSSFWorkbook(fs);
+                    this.Alert("没有在文件“" + Path.GetFileName(filepath) + "”中找到“单据编号”列");
+                    return null;
                 }
-                else if (filepath.IndexOf(".xls", StringComparison.Ordinal) > 0) // 2003版本
+                //     sheet.GetRow(1).RowStyle.FillBackgroundColor
+                int rowCount = sheet.LastRowNum;
+                //为什么用<= rowCount?因为LastRowNum是从0开始计算，所以真正的行数应该是LastRowNum+1
+                for (int i = START_ROW_INDEX; i <= rowCount; i++)
                 {
-                    workbook = new HSSFWorkbook(fs);
+
+                    var cell = sheet.GetRow(i).GetCell(colIndex);
+                    if (cell == null)
+                    {
+                        break;
+                    }
+                    results.Add(cell.StringCellValue);
                 }
+                return results;
+            }
+            catch (IOException ioEx)
+            {
+                this.Alert("读取Excel出错 => " + ioEx.Message);
             }
             catch (Exception ex)
             {
-                this.Alert("读取Excel出错=>" + ex.Message);
+                this.Alert("获取单据号失败 => " + ex.Message);
             }
-            var sheet = workbook.GetSheetAt(0);
-            int startRowIndex = 1; //为什么不是0？因为首行是列头
-            int colIndex = 1;//默认第二列是单据编号
-            int rowCount = sheet.LastRowNum;
-            for (int i = startRowIndex; i < rowCount; i++)
+            finally
             {
-                results.Add(sheet.GetRow(i).GetCell(colIndex).StringCellValue);
+                if (fs != null) fs.Dispose();
             }
-            return results;
+            return null;
         }
-//        function expect(arr1, arr2) {
-//    var results = [];
-//    for (var i = 0, len = arr1.length; i < len; i++) {
-//        if (arr2.indexOf(arr1[i]) < 0) {
-//            results.push(arr1[i]);
-//        }
-//    }
-//    return results;
-//}
 
-        private List<string> GetNoInListOneAndNotInListTwo(List<string> list1, List<string> list2)
+        private void FillBackgroundColor(string filepath, IList<string> numberList)
         {
-            var results = new List<string>();
-            foreach (var str in list1)
+            this.AddLog("正在对差异文件进行着色");
+            var workbook = this.GetWorkbook(filepath);
+            var sheet = workbook.GetSheetAt(0);
+            var rowCount = sheet.LastRowNum;
+
+            var cellStyle = workbook.CreateCellStyle();
+            cellStyle.FillForegroundColor = HSSFColor.LightGreen.Index;
+            cellStyle.FillPattern = FillPattern.SolidForeground;
+
+            int colIndex = this.GetDocumentNumberColIndex(sheet);
+            int colorRowCount = 0;
+            ICell cell;
+            for (int i = START_ROW_INDEX; i <= rowCount; i++)
             {
-                if (list2.IndexOf(str) < 0)
+                cell = sheet.GetRow(i).GetCell(colIndex);
+                if (numberList.Contains(cell.StringCellValue.Trim()))
                 {
-                    results.Add(str);
+                    cell.CellStyle = cellStyle;
+                    colorRowCount++;
                 }
             }
-            return results;
+            var destPath = Path.GetDirectoryName(filepath) + "/" + Path.GetFileName(filepath).Replace(".xls", "_校对结果.xls");
+            var fsDist = new FileStream(destPath, FileMode.Create);
+            workbook.Write(fsDist);
+            fsDist.Flush();
+            fsDist.Close();
+            if(DialogResult.Yes == MessageBox.Show("已将差异单据在试剂部文件中着色，共标记行数："+ colorRowCount +"，是否打开结果文件？","提示",MessageBoxButtons.YesNo))
+            {
+                System.Diagnostics.Process.Start(destPath);
+            }
         }
         #endregion
 
@@ -115,26 +178,40 @@ namespace ClumsyAssistant.Pages
             }
         }
 
-        #endregion
-
         private void BtnStart_Click(object sender, EventArgs e)
         {
             if (TbFile.Text == "" || !File.Exists(TbFile.Text))
             {
-                this.Alert("请选择同兴源出入库文件。");
+                this.Alert("请选择同兴源出/入库文件。");
                 return;
             }
             if (TbFile2.Text == "" || !File.Exists(TbFile2.Text))
             {
-                this.Alert("请选择试剂部出入库文件。");
+                this.Alert("请选择试剂部出/入库文件。");
                 return;
             }
+            logBuilder.Clear();
+            BtnStart.Enabled = false;
+            this.AddLog("开始校对数据");
 
-            var documentNumberList = this.GetDocumentNumber(TbFile.Text).Distinct().ToList();
-            var documentNumber2List = this.GetDocumentNumber(TbFile2.Text).Distinct().ToList();
+            var txyDocNoList = this.GetDocumentNumber(TbFile.Text);
+            var sjbDocNoList = this.GetDocumentNumber(TbFile2.Text);
 
-            var numberList = documentNumber2List.Except(documentNumberList).ToList();
-            Alert(numberList.Count.ToString());
+            if (txyDocNoList != null && sjbDocNoList != null)
+            {
+                this.AddLog("同兴源出/入库文件单据总数：" + txyDocNoList.Count);
+                this.AddLog("试剂部出/入库文件单据总数：" + sjbDocNoList.Count);
+                txyDocNoList = txyDocNoList.Distinct().ToList();
+                this.AddLog("同兴源出/入库文件单据去重后总数：" + txyDocNoList.Count);
+                sjbDocNoList = sjbDocNoList.Distinct().ToList();
+                this.AddLog("试剂部出/入库文件单据去重后总数：" + sjbDocNoList.Count);
+                var noList = sjbDocNoList.Except(txyDocNoList).ToList();
+                this.Alert("试剂部出/入库文件与同兴源出/入库文件单据号差异数：" + noList.Count);
+                this.FillBackgroundColor(TbFile2.Text, noList);
+                this.AddLog("校对完成，(^.^)");
+            }
+            BtnStart.Enabled = true;
         }
+        #endregion
     }
 }
